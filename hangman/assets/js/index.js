731ece3ef9ed6f244274269f60051f29b52b2641
Vitaly Dushkin <vetalapo@gmail.com>
2024-01-26 03:05:54 +0100

    feat: render base container for the game

--- hangman/assets/js/index.js
+++ hangman/assets/js/index.js
@@ -1,7 +1,36 @@
 "use strict";
 
+class Hangman {
+    constructor() {
+        this.renderMainContainer();
+    }
+
+    renderMainContainer() {
+        // Game container
+        const container = document.createElement("div");
+        container.classList.add("hangman-game-container");
+
+        // Gallows container
+        const gallows = document.createElement("div");
+        gallows.id = "gallows-container";
+
+        // Quiz container
+        const quiz = document.createElement("div");
+        quiz.id = "quiz-container";
+
+        container.appendChild(gallows);
+        container.appendChild(quiz);
+
+        // Rendering containers
+        document.body.appendChild(container);
+    }
+
+}
+
 // On load
 (() => {
+    const hangman = new Hangman();
+
     console.info("Welcome to the Hangman World!");
     console.info("Make yourself comfortable while you're here. ♥");
 })();
