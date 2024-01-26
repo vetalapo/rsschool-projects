f622afc8e66cfc0769304bd5177f9082fb61cf2d
Vitaly Dushkin <vetalapo@gmail.com>
2024-01-26 21:49:16 +0100

    feat: implement gallows display and cyclin

--- hangman/assets/js/index.js
+++ hangman/assets/js/index.js
@@ -2,6 +2,7 @@
 
 class Hangman {
     constructor() {
+        this.gallows = null;
         this.renderMainContainer();
     }
 
@@ -11,20 +12,68 @@ class Hangman {
         container.classList.add("hangman-game-container");
 
         // Gallows container
-        const gallows = document.createElement("div");
-        gallows.id = "gallows-container";
+        const gallowsContainer = document.createElement("div");
+        gallowsContainer.id = "gallows-container";
+        this.gallows = new Gallows(gallowsContainer);
 
         // Quiz container
         const quiz = document.createElement("div");
         quiz.id = "quiz-container";
 
-        container.appendChild(gallows);
+        container.appendChild(gallowsContainer);
         container.appendChild(quiz);
 
         // Rendering containers
         document.body.appendChild(container);
     }
+}
 
+class Gallows {
+    constructor(container) {
+        this.pictureId = "hangman-state";
+        this.state = 0;
+
+        const image = document.createElement("img");
+        image.id = this.pictureId;
+        image.src = "assets/image/state/0.png";
+        image.alt = "Hangman slide state picture"
+
+        container.appendChild(image);
+    }
+
+    setState(state) {
+        const image = document.getElementById(this.pictureId);
+        image.src = `assets/image/state/${state}.png`;
+    }
+
+    cycleState() {
+        if (this.state === "win") {
+            return;
+        }
+        
+        this.state++;
+
+        if (this.state >= 7) {
+            this.state = 6;
+        }
+
+        this.setState(this.state);
+    }
+
+    gameReset() {
+        this.state = 0;
+        this.setState(this.state);
+    }
+
+    gameOver() {
+        this.state = 6;
+        this.setState(this.state);
+    }
+
+    gameWin() {
+        this.state = "win";
+        this.setState(this.state);
+    }
 }
 
 // On load
